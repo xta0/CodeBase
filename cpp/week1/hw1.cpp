e6ed7db480d980075df0f07e1b157c783cd3f32c
xta0 <jayson.xu@foxmail.com>
2018-01-04 18:37:40 -0500

    update

---
+++ cpp/week1/hw1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a = 0;
+    cin<<"enter number (1-10): "<< a;
+    if ( a < 1 || a>10)
+    {
+        cout <<"number out of range";
+    }
+    else{
+
+    }
+
+    return 0;
+}
