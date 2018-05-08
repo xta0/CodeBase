9b2d621b29faf1cd4aafe71955dadd5721f41cbb
xta0 <jayson.xu@foxmail.com>
2018-05-08 17:49:25 -0400

    update

--- cpp/algo/bits.cpp
+++ cpp/algo/bits.cpp
@@ -1,40 +1,61 @@
 #include <iostream>
 using namespace std;
-//https://www.quora.com/How-do-I-divide-two-numbers-using-only-bit-operations-in-Java
 
 int add(int a, int b)
 {
-    while(b!=0){
-
-    }
     int partialSum, carry;
     do{
-        cout<<"a: "<<a<<", b: "<<b<<endl;
         partialSum = a ^ b;
         carry = (a & b) << 1;
         a = partialSum;
-        b = carry;
-        cout<<"a: "<<a<<", b: "<<b<<endl;
-        
+        b = carry; 
     } while (carry != 0);
     return partialSum;
 }
+int substract(int a, int b){
 
+    return add(a,add(~b,1));
+}
+int multiply(int a, int b){
+    int x = a < 0 ? add(~a, 1) : a; //取绝对值
+    int y = b < 0 ? add(~b, 1) : b; //取绝对值
+    
+    int result = 0;
+    while(y){
+        if(x&0x01){
+            result = add(result,x);
+        }
+        x<<1;
+        y>>1;
+    }
+    if((a^b)<0){
+        result = add(~result,1);
+    }
+    return result;
+
+}
+int divide(int a, int b){
+    int dividend = a < 0 ? add(~a, 1) : a; //取绝对值
+    int divisor = b < 0 ? add(~b, 1) : b; //取绝对值
+    int remainder = dividend;
+    int result = 0;
+    while(remainder >= divisor){
+        remainder = substract(remainder,divisor);
+        result = add(result,1);
+    }
+    if((a^b)<0){
+        result = add(~result,1);
+    }
+    return result;
+}
 int main()
 {
-    int a=1;
-    int b=3;
-    cout<<(a^b)<<","<<(a|b)<<", "<<(~a)<<endl;
+    int a=2; int b=-11;
+    cout<<add(~b,1)<<endl;
+    cout<<substract(3,5)<<endl;
+    cout<<multiply(2,3)<<endl;
+    cout<<divide(7,3)<<endl;
 
-    while(true){
-        int a,b;
-        cin>>a>>b;
-        if(a==0&b==0){
-            break;
-        }else{
-            add(a,b);
-        }
-    }
-    
+
     return 0;
 }
