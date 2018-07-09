ee23130ddf2c0f948141614b5cfe590469a28c94
xta0 <jayson.xu@foxmail.com>
2018-07-09 17:28:38 -0400

    update

--- python/algo/fib.py
+++ python/algo/fib.py
@@ -1,18 +1,32 @@
+#Naive recurrsion
 def fib1(n):
     if n==0 or n==1:
         return n
     else:
         return fib1(n-1)+fib1(n-2)
 
-print(fib1(30))
+#Recursion + Memoization
+def fib2(n,memo):
+    if n in memo:
+        return memo[n]
+    else:
+        if n==0 or n==1:
+            return n
+        else:
+            v = fib2(n-1,memo)+fib2(n-2,memo)
+            memo[n] = v
+            return v
 
-def fib2(n):
-    l = list(range(0,n+1))
-    l[0] = 0
-    l[1] = 1
+#DP
+def fib3(n):
+    fib ={} 
+    fib[0] = 0
+    fib[1] = 1
     for i in range(2,n+1):
-        l[i] = l[i-1]+l[i-2]
+        fib[i] = fib[i-1]+fib[i-2]
 
-    return l[n]
+    return fib[n]
 
-print(fib2(30))
+print(fib1(30))
+print(fib2(30,{}))
+print(fib3(30))
