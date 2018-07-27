f92e19eb4686f312cdc3a09ae0310a359eee078d
xta0 <jayson.xu@foxmail.com>
2018-07-27 15:17:28 -0400

    update

--- python/parser/lexer.py
+++ python/parser/lexer.py
@@ -1,22 +1,29 @@
+# Python LA library
+import ply.lex as lex
 import re 
 
+tokens = (
+    'LANGLE', # <
+)
+# HTML Tokens
 def t_WHITESPACE(stoken):
     regexr = r' '
     pass
 
 def t_STRING(token):
     regexr = r'"[^"]+"'
-    re.findall(regexr,token)
     return token
 
 def t_NUMBER(token):
     regexr = r'[0-9]+'
-    re.findall(regexr,token)
     return token
 
 def t_WORD(token):
     regexr = r'[^ <>]+'
-    re.findall(regexr,token)
     return token        
 
-
+# Javascript tokens
+def t_commnet(token):
+    r'//[^\n]'
+def t_numbers(token):
+    r'-?[0-9]+(?:\.[0-9]+)?'
