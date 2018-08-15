3c7918d1d29f5412d5a42f33505bcd42f7b8061c
xta0 <jayson.xu@foxmail.com>
2018-08-15 18:33:12 -0400

    Update(auto commit)

--- nodejs/.eslintrc.js
+++ nodejs/.eslintrc.js
@@ -10,23 +10,6 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
         'no-console': 'off'
-        
     }
 };
