077919b4d1319fb3609f65af8cf0a125c2901c16
xta0 <jayson.xu@foxmail.com>
2018-09-10 00:49:53 -0400

    Update(auto commit)

--- javascript/functions/arguments.js
+++ javascript/functions/arguments.js
@@ -1,14 +1,21 @@
 function func() {
-    console.log(arguments[0]) //1
+  console.log(arguments[0]); //1
 }
-func(1, 2, 3)
+func(1, 2, 3);
+
+function listArguments() {
+  return arguments;
+}
+
+var args = listArguments(1, 2, 3); // type : object
+console.log(args); //{ '0': 1, '1': 2, '2': 3 } //seudo array
 
 function sum() {
-    let total = 0;
-    for (const argument of arguments) {
-        total += argument;
-    }
-    return total;
+  let total = 0;
+  for (const argument of arguments) {
+    total += argument;
+  }
+  return total;
 }
 
-console.log(sum(1, 23, 3, 4))
+console.log(sum(1, 23, 3, 4));
