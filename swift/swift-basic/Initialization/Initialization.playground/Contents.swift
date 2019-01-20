8626bdedd8671c9fba02c98f3dfbf6273c670989
xta0 <jayson.xu@foxmail.com>
2019-01-20 13:42:47 -0500

    Update(auto commit)

---
+++ swift/swift-basic/Initialization/Initialization.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+import Foundation
+
+/*
+ 1. Swift 类的构造函数没有返回值
+ 2. Class和Struct的store property必须初始化
+ */
+
---
+++ swift/swift-basic/Properties/Properties.playground/Contents.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+/*
+ Two types of properties
+ 
+ 1. Store Property
+    - struct
+    - class
+ 2. Computed Property
+    - struct
+    - class
+    - enums
+
+ */
+
+/*
+ ====
+ Store Properties
+ ====
+ */
+struct FixedLengthRange{
+    var firstValue: Int
+    let length: Int //let is constant
+}
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue - 6;
+
+
+
+
+/*
+ ====
+ Computed Properties
+ ====
+ */
+class T{
+    
+    //readonly
+    var foo: Double {
+        get{
+            return 1.0
+        }
+        set ( newValue ) {}
+    }
+}
+
+
+struct Card{
+    var selected:Bool = false
+//    init(selected: Bool){
+//        self.selected = selected
+//    }
+}
+
+let cards:[Card] = [Card(selected: true),Card(selected: true)]
+
+for var card in cards {
+    card.selected = true
+}
+
+print(cards[1].selected)
+
+
+/*
+ ====
+ Properties Observers
+ ====
+ */
+
+//lazy var does not have didset mathod
