dc81783a6f96b525fe3285214f9a6698affdfcc7
Tao Xu <taox@fb.com>
2019-12-01 11:50:25 -0800

    Update(auto commit)

--- cpp/pointer/smart_ptr.cpp
+++ cpp/pointer/smart_ptr.cpp
@@ -9,20 +9,48 @@ using namespace std;
 template<typename T>
 class smart_ptr {
 public:
+    template<typename U>
+    friend class smart_ptr;
     explicit smart_ptr(T* ptr = nullptr):ptr_(ptr){}
-    ~smart_ptr() { delete _ptr; }
-    T* get() const { return _ptr; };
-    T& operator*() const { return *_ptr; }
-    T* operator->() const { return _ptr; }
-    operator bool() const { return _ptr; }
+    //copy constructor
+    smart_ptr(smart_ptr& other) {
+        ptr_ = other.release();
+    }
+    //move constructor
+    // smart_ptr(smart_ptr&& other) {
+
+    // }
+    ~smart_ptr() { delete ptr_; }
+    T* get() const { return ptr_; };
+    T* release() {
+        T* ptr = ptr_;
+        ptr_ = nullptr;
+        return ptr;
+    }
+    T& operator*() const { return *ptr_; }
+    T* operator->() const { return ptr_; }
+    operator bool() const { return ptr_; }
+    smart_ptr& operator=(smart_ptr& other) {
+        std::swap(this, other->ptr_);
+        return *this;
+    }
 private:
-    T* _ptr;
+    T* ptr_;
+
 };
 
 int main(){
 
-
-
-
+    smart_ptr<int> ptr1{new int(111)};
+    int x = *ptr1;d'k'gu'g'ilginngbchbkrjgcifnfrrcvnte
+    cout<<x<<endl;
+    smart_ptr<int> ptr2 = ptr1;
+    x = *ptr2;
+    cout<<x<<endl;
+    smart_ptr<int> ptr3{ptr2};
+    x = *ptr3;
+    cout<<x<<endl;
+    x = *ptr2;
+    cout<<x<<endl;
     return 0;
 }
