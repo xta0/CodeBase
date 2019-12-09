ef6623d4db7649aa1cf353e6090b63828a8afd3c
Tao Xu <taox@fb.com>
2019-12-09 10:52:48 -0800

    Update(auto commit)

---
+++ cpp/test/bar.h
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+inline void func3(){
+    cout<<"func3 from bar"<<endl;
+}
--- cpp/test/foo.h
+++ cpp/test/foo.h
@@ -6,3 +6,10 @@ void func1(T x) {
     cout<<x<<endl;
 }
 void func2();
+inline void func3() {
+    cout<<"func3"<<endl;
+}
+//this will cause duplicated symbols
+void func4(){
+    cout<<"func4"<<endl;
+}
