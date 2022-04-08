c812603a82587833d591f12eddbb8c3935846a9e
Tao Xu <taox@fb.com>
2022-04-08 09:34:05 -0700

    Update(auto commit)

---
+++ rust/target/debug/deps/hello_world-fc5c79d61dde6809.d
@@ -0,0 +1,5 @@
+/Users/taox/Projects/CodeBase/rust/target/debug/deps/hello_world-fc5c79d61dde6809.rmeta: src/main.rs
+
+/Users/taox/Projects/CodeBase/rust/target/debug/deps/hello_world-fc5c79d61dde6809.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/taox/Projects/CodeBase/rust/target/debug/hello_world: /Users/taox/Projects/CodeBase/rust/src/main.rs
