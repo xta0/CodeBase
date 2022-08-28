4cd5ff1d5e5054ccf00e5172684951da42a39528
Tao Xu <taox@fb.com>
2022-08-28 09:54:45 -0700

    Update(auto commit)

---
+++ swift/swift-module/Logger.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public class Logger {
+    
+    var prefix: String = ""
+    
+    public init(prefix:String) {
+        self.prefix = prefix
+    }
+    public func log<T>(object: T)  {
+        print(prefix + " \(object)")
+    }
+
+    @objc public func objc_log(object: String) {
+        print(prefix + "[OBJC]" + " \(object)")
+    }
+}
