5ebb3382283be2aad10b7ee7c4ce0f544a328387
Tao Xu <taox@fb.com>
2023-10-01 15:28:41 -0700

    Update(auto commit)

---
+++ swift/swiftui-example/iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Tao Xu on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
