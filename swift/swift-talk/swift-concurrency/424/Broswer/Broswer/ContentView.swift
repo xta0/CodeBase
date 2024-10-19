d2dcf4200a8ed4f5a0f89cab4f89f451cb0cba3f
1PSDK User <1psdk@1psdk.com>
2024-10-19 12:37:48 -0700

    Update(auto commit)

--- swift/swift-talk/swift-concurrency/424/Broswer/Broswer/ContentView.swift
+++ swift/swift-talk/swift-concurrency/424/Broswer/Broswer/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import Observation
+import WebKit
+
 
 struct Page: Identifiable, Hashable {
     var url: URL
@@ -14,24 +16,58 @@ struct Page: Identifiable, Hashable {
 }
 
 @Observable
-@MyGlobalActor
 class Store: Observable {
-    var pages: [Page] = []
+    var pages: [Page] = [
+        Page(url: .init(string: "https://www.objc.io")!),
+        Page(url: .init(string: "https://www.apple.com")!)
+    ]
     nonisolated init() {}
     func submit(_ url: URL) {
         pages.append(Page(url: url))
     }
 }
 
-@globalActor
-actor MyGlobalActor {
-    static let shared = MyGlobalActor()
+struct WebView: NSViewRepresentable {
+    var url: URL
+    var snapshot: (_ takeSnapshot: @escaping () async -> NSImage) -> ()
+    class Coordinator: NSObject, WKNavigationDelegate {
+        
+    }
+    func makeCoordinator() -> Coordinator {
+        .init()
+    }
+    func makeNSView(context: Context) -> WKWebView {
+        let result = WKWebView()
+        result.navigationDelegate = context.coordinator
+        return result
+    }
+    func updateNSView(_ nsView: WKWebView, context: Context) {
+        assert(Thread.isMainThread)
+        snapshot({
+            do {
+                assert(Thread.isMainThread)
+                let image = try await nsView.takeSnapshot(configuration: nil)
+                assert(Thread.isMainThread)
+                return image
+            } catch {
+                return NSImage()
+            }
+        })
+        if nsView.url != url {
+            let request = URLRequest(url: url)
+            nsView.load(request)
+        }
+    }
 }
+
+extension NSImage: @unchecked Sendable {}
 
 struct ContentView: View {
     @State var store = Store()
     @State var currentURLString: String = "https://sg.1psdk.com"
     @State var selectedPage: Page.ID?
+    @State var image:NSImage?
+    @State var takeSnapshort: (() async ->NSImage)?
     
     var body: some View {
         NavigationSplitView {
@@ -41,8 +77,17 @@ struct ContentView: View {
                 }
             }
         } detail: {
-            if let s = selectedPage {
-                Text(s.uuidString)
+            if let s = selectedPage, let page = store.pages.first(where: {$0.id == s}) {
+                WebView(url: page.url) { takeSnapshot in
+                    self.takeSnapshort = takeSnapshot
+                }
+                .overlay {
+                    if let i = image {
+                        Image(nsImage: i)
+                            .scaleEffect(0.5)
+                            .border(Color.red)
+                    }
+                }
             } else {
                 ContentUnavailableView("No Page Selected!", systemImage: "globe")
             }
@@ -52,15 +97,20 @@ struct ContentView: View {
                 TextField("URL", text: $currentURLString)
                     .onSubmit {
                         if let url = URL(string: currentURLString) {
-                            Task {
-                                await store.submit(url)
-                                currentURLString = ""
-                            }
+                            currentURLString = ""
+                            store.submit(url)
                         }
                     }
             }
+            ToolbarItem {
+                Button("Take a snapshot") {
+                    Task {
+                        image = await self.takeSnapshort?()
+                    }
+                }
+            }
         }
-
+        
     }
 }
 
